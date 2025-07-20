eceb9c0af104e788788cbfd8286db64a4021cf81
Button99 <christoskoubis@yahoo.com>
2025-07-20 18:02:19 +0300

    More solutions

---
+++ PowerOfFour.php
@@ -0,0 +1,35 @@
+<?php
+
+// Given an integer n, return true if it is a power of four. Otherwise, return false.
+
+// An integer n is a power of four, if there exists an integer x such that n == 4x.
+
+ 
+
+// Example 1:
+
+// Input: n = 16
+// Output: true
+// Example 2:
+
+// Input: n = 5
+// Output: false
+// Example 3:
+
+// Input: n = 1
+// Output: true
+ 
+
+class PowerOfFour {
+    function isPowerOfFour($n) {
+            if($n == 1) {
+                return true;
+            }
+            for($i=1; $i<=$n; $i*=4) {
+                if($n == $i) {
+                    return true;
+                }
+            }
+            return false;
+    }
+}
